55a09f5256f983f9311c33d1a63763fc5c6a47a9
zhangyang891008 <zhangyang891008@gmail.com>
2019-05-02 15:19:33 +0800

    26. 删除排序数组中的重复项
    Change-Id: I36b2ae53b17da37a198abe34fbcaea49448c3426
    Signed-off-by: zhangyang891008 <zhangyang891008@gmail.com>

---
+++ src/com26/removeDuplicates/RemoveDuplicates.java
@@ -0,0 +1,52 @@
+package com26.removeDuplicates;
+
+/**
+ * 26. 删除排序数组中的重复项
+ * @author zy
+ *给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+
+不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+
+示例 1:
+
+给定数组 nums = [1,1,2], 
+
+函数应该返回新的长度 2, 并且原数组 nums 的前两个元素被修改为 1, 2。 
+
+你不需要考虑数组中超出新长度后面的元素。
+示例 2:
+
+给定 nums = [0,0,1,1,1,2,2,3,3,4],
+
+函数应该返回新的长度 5, 并且原数组 nums 的前五个元素被修改为 0, 1, 2, 3, 4。
+
+你不需要考虑数组中超出新长度后面的元素。
+
+ */
+public class RemoveDuplicates {
+	public static int removeDuplicates(int[] nums) {
+		int size = 0;
+        if(nums.length ==0) {
+        	return 0;
+        }
+        if(nums.length == 1) {
+        	return 1;
+        }
+        
+        int start = 0;
+        size++;
+        int next = 1;
+        for(;next<nums.length;next++) {
+        	if(nums[start]!=nums[next]) {
+        		size++;
+        		nums[++start] = nums[next];
+        	}
+        }
+        return size;
+    }
+	public static void main(String[] args) {
+		int arr[]= {0,2,3,3,4,4,6,7,7};
+		int ret = removeDuplicates(arr);
+		System.out.println(ret);
+	}
+}
