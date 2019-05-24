c790ffca23ecf3800d1daa476ccb3338b5d88ec5
zhangyang891008 <zhangyang891008@gmail.com>
2019-05-24 21:19:48 +0800

    visitor模式

---
+++ src/com86/partition/Partition.java
@@ -0,0 +1,21 @@
+package com86.partition;
+
+public class Partition {
+	public static ListNode partition(ListNode head, int x) {
+		ListNode small = head;
+		ListNode temp = head;
+		
+		
+		
+        return head;
+    }
+	public static void main(String[] args) {
+		
+	}
+
+}
+class ListNode {
+	 int val;
+	 ListNode next;
+	 ListNode(int x) { val = x; }
+}
